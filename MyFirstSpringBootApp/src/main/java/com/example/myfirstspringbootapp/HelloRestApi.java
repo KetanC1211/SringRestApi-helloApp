e2d41585b66f29ab13ebd7bafc156985165b2eb7
KetanC1211 <ketanchindarkar12@gmail.com>
2022-09-14 22:43:04 +0530

    Added Post mapping , passed json obj to display msg

--- MyFirstSpringBootApp/src/main/java/com/example/myfirstspringbootapp/HelloRestApi.java
+++ MyFirstSpringBootApp/src/main/java/com/example/myfirstspringbootapp/HelloRestApi.java
@@ -4,20 +4,21 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloRestApi {
-
     @RequestMapping("/hello")
     public String printHello() {
         return "hello world trying requesting Mapping";
     }
-
     @RequestMapping(value = {"/query"},method = RequestMethod.GET)
     public String sayHello(@RequestParam(value = "name")String Name){
         return "Hello " + Name+ " from BridgeLabs.";
     }
-
     @RequestMapping("/param/{name}")
     public String sayHelloParam(@PathVariable String name){
         return "Hello "+name+ " from BridgeLabs.";
+    }
+    @PostMapping ("/post")
+    public String printMsg (@RequestBody User u){
+        return "Hello "+u.getFirstName()+" "+u.getLastName()+ " from BridgeLabs.";
     }
 
 }
