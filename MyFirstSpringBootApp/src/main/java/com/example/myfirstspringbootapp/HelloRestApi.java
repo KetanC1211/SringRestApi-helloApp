64cef83cdfb589391ef2f7957e93172d7be0c0a8
KetanC1211 <ketanchindarkar12@gmail.com>
2022-09-14 22:07:29 +0530

    passing name as query parameter, using rest api call

--- MyFirstSpringBootApp/src/main/java/com/example/myfirstspringbootapp/HelloRestApi.java
+++ MyFirstSpringBootApp/src/main/java/com/example/myfirstspringbootapp/HelloRestApi.java
@@ -1,13 +1,23 @@
 package com.example.myfirstspringbootapp;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRestApi {
 
-    @RequestMapping("/hello")
-    public String printHello() {
-        return "hello world trying requesting Mapping";
+//    @RequestMapping("/hello")
+//    public String printHello() {
+//        return "hello world trying requesting Mapping";
+//    }
+
+    @RequestMapping(value = {"/query"},method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value = "name")String Name){
+        return "Hello " + Name+ " from BridgeLabs.";
     }
+
+
+
 }
