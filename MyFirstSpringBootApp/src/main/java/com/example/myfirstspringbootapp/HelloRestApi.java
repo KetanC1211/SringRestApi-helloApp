e5715c4b815ebe146bc765377cdd068a89468a70
KetanC1211 <ketanchindarkar12@gmail.com>
2022-09-14 22:25:18 +0530

    Added name using param

--- MyFirstSpringBootApp/src/main/java/com/example/myfirstspringbootapp/HelloRestApi.java
+++ MyFirstSpringBootApp/src/main/java/com/example/myfirstspringbootapp/HelloRestApi.java
@@ -1,23 +1,23 @@
 package com.example.myfirstspringbootapp;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloRestApi {
 
-//    @RequestMapping("/hello")
-//    public String printHello() {
-//        return "hello world trying requesting Mapping";
-//    }
+    @RequestMapping("/hello")
+    public String printHello() {
+        return "hello world trying requesting Mapping";
+    }
 
     @RequestMapping(value = {"/query"},method = RequestMethod.GET)
     public String sayHello(@RequestParam(value = "name")String Name){
         return "Hello " + Name+ " from BridgeLabs.";
     }
 
-
+    @RequestMapping("/param/{name}")
+    public String sayHelloParam(@PathVariable String name){
+        return "Hello "+name+ " from BridgeLabs.";
+    }
 
 }
